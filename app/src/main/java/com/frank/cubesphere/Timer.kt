4f78ea4a5b6e6ed0dd4ce2987d4b67cabd263918
李強 <30677343+FrankLiQiang@users.noreply.github.com>
2024-08-05 16:17:38 +0900

    追加SurfaceView相关资料

--- app/src/main/java/com/frank/cubesphere/Timer.kt
+++ app/src/main/java/com/frank/cubesphere/Timer.kt
@@ -75,3 +75,11 @@ fun stopDrawTimer() {
     }
 }
 
+//https://developer.android.com/reference/kotlin/android/view/SurfaceView
+//https://qiita.com/ymshun/items/a1447bdfcea8ef24d765
+//https://stackoverflow.com/questions/19019825/android-setpixels-explanation-and-example
+//https://weiwangqiang.github.io/2020/12/07/surfaceview-profile-animator/
+//https://qiita.com/yoshihiro-kato/items/7ad1202891e183df4e86
+//https://www.bing.com/search?q=kotlin+bitmap.setPixels+%E5%87%BD%E6%95%B0&qs=n&form=QBRE&sp=-1&lq=0&pq=kotlin+bitmap.setpixels+%E5%87%BD%E6%95%B0&sc=11-26&sk=&cvid=53D52BD46F7D4D378849CFEBCCA1EFD0&ghsh=0&ghacc=0&ghpl=
+//https://www.bing.com/search?pglt=41&q=Kotlin+%E5%88%A9%E7%94%A8Canvas+%E5%92%8C+SurfaceView+%E5%B9%B3%E6%BB%91%E6%98%BE%E7%A4%BA%E5%8A%A8%E7%94%BB&cvid=829c16feb2d0431faeb84164d5dffcf5&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MggIARDpBxj8VdIBCDI0NzZqMGoxqAIAsAIA&FORM=ANNAB1&adppc=EDGEDBB&PC=EDGEDBB
+//https://yskskt.hatenablog.com/entry/2023/10/03/215430
